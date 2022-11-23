0e64a8b8a8f9f6f200f1c1b1cf8abd1311ecf30e
Ryan Chisholm <71892582+Chisholm6192@users.noreply.github.com>
2022-11-23 13:08:07 -0500

    Create Task2.py

---
+++ Assignments/Assignment 1/Task2.py
@@ -0,0 +1,17 @@
+"""
+-------------------------------------------------------
+Assignment 1 Task 2
+-------------------------------------------------------
+Author:  Ryan Chisholm
+ID:      169027577
+Email:   chis7577@mylaurier.ca
+__updated__ = "2022-09-26"
+-------------------------------------------------------
+"""
+# Imports
+
+# Constants
+    
+fav_food = str(input("Enter your favourite food: ")) #input for favourite food
+TV_Show = str(input("Enter your favourite TV show: ")) #input for favourite TV Show
+print(f"I like eating {fav_food} while watching {TV_Show}")
