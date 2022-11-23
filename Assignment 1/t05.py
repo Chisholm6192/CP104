4efba748e11668ddbb31f0f2fe5a28c631c82008
Ryan Chisholm <71892582+Chisholm6192@users.noreply.github.com>
2022-11-23 12:56:15 -0500

    Update t05.py

--- Assignment 1/t05.py
+++ Assignment 1/t05.py
@@ -1,1 +1,24 @@
+"""
+-------------------------------------------------------
+Assignment 1 Task 5
 
+Program takes mortgage amount, interest, length of mortgage(in years), and number of times the
+interest is compounded per year, and outputs the total amount owing for the mortgage, with interest included
+-------------------------------------------------------
+Author:  Ryan Chisholm
+ID:      169027577
+Email:   chis7577@mylaurier.ca
+__updated__ = "2022-09-26"
+-------------------------------------------------------
+"""
+# Imports
+
+# Constants
+
+P = float(input("Principal: $")) #principal mortgage amount
+r = float(input("Interest (decimal): ")) #annual interest rate
+t = int(input("Number of years: ")) #number of years that the mortgage is for
+n = int(input("Number of times interest compounded per year: ")) #how many times the interest is compounded per year
+    
+A = P*((1 + (r/n))**(n*t)) #calculation to find the balance owing, after interest is added in
+print(f"Balance: $ {A}")
